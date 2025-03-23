8b8bf1db2a68cac86e6ddfa845cc793b0857f24d
Mattferreira1 <matheus2100discord@gmail.com>
2025-03-23 13:54:48 -0300

    .

--- src/Models/BookModel.js
+++ src/Models/BookModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true }, // Título do livro
-  author: { type: String, required: true }, // Autor
-  genre: { type: String }, // Gênero
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String },
   url:{type: String}
 });
 
